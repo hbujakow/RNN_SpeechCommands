84bddfb5b0831fca5fc11ef529cfb2a4a6e587cd
galkowskim <mikolaj.galkowski7@gmail.com>
2024-04-28 18:53:57 +0200

    update files

--- experiments/LSTM/lstm.py
+++ experiments/LSTM/lstm.py
@@ -1,18 +1,29 @@
 import json
-from datetime import datetime
+import os
 
+import librosa
 import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
 from datasets import load_dataset
-from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# device = 'cuda:3'
 subset_of_labels = ["up", "down", "left", "right", "on", "off", "yes", "no"]
+
+
+def compute_mfcc(data, sample_rate=16000, n_mfcc=12):
+    # Extract MFCC features
+    # https://librosa.github.io/librosa/generated/librosa.feature.mfcc.html
+    mfcc = librosa.feature.mfcc(
+        y=data,
+        sr=sample_rate,
+        n_mfcc=n_mfcc,  # How many mfcc features to use? 12 at most.
+        # https://dsp.stackexchange.com/questions/28898/mfcc-significance-of-number-of-features
+    )
+    return mfcc
 
 
 def extract_fields(example):
@@ -43,7 +54,12 @@ def preprocess(speech_data):
     )
     test = test.map(remove_columns=["file", "audio", "speaker_id", "utterance_id"])
 
-    return train, validation, test
+    return (
+        train.with_format("torch"),
+        validation.with_format("torch"),
+        test.with_format("torch"),
+    )
+
 
 class LSTMModel(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size):
@@ -64,9 +80,9 @@ class LSTMModel(nn.Module):
 
 
 def calculate_accuracy(preds, y):
-    temp = torch.nn.functional.softmax(preds, dim=1)
-    temp = torch.argmax(temp, dim=1)
-    return (torch.sum(temp == y) / len(y)).item()
+    preds = torch.nn.functional.softmax(preds, dim=1)
+    preds = torch.argmax(preds, dim=1)
+    return (torch.sum(preds == y) / len(y)).item()
 
 
 def main(config):
@@ -74,8 +90,11 @@ def main(config):
     NUM_EPOCHS = config["num_epochs"]
     SEED = config["seed"]
     NUM_LAYERS = config["num_layers"]
+    HIDDEN_SIZE = config["hidden_size"]
 
-    speech_data = load_dataset("speech_commands", "v0.02")
+    speech_data = load_dataset(
+        "speech_commands", "v0.02", cache_dir="only_selected/data_here"
+    )
 
     train, validation, test = preprocess(speech_data)
 
@@ -84,9 +103,21 @@ def main(config):
     test = test.shuffle(seed=SEED)
 
     torch.cuda.empty_cache()
-    model = LSTMModel(16_000, 64, NUM_LAYERS, len(subset_of_labels))
+    model = LSTMModel(
+        12, HIDDEN_SIZE, NUM_LAYERS, len(subset_of_labels)
+    )  # 12 - corresponds to 12 features in MFCC
 
     model.to(device)
+
+    model_path = (
+        f"{model._get_name()}" + f"__NUM_LAY_{NUM_LAYERS}_HID_{HIDDEN_SIZE}_SEED_{SEED}"
+    )
+
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+
+    with open(f"{model_path}/config.json", "w") as f:
+        json.dump(config, f, indent=4)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
@@ -101,6 +132,7 @@ def main(config):
 
     train_loader = DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
     valid_loader = DataLoader(validation, batch_size=BATCH_SIZE)
+    test_loader = DataLoader(test, batch_size=BATCH_SIZE)
 
     for epoch in tqdm(range(1, NUM_EPOCHS + 1)):
         model.train()
@@ -108,9 +140,11 @@ def main(config):
         train_accuracy = 0
         for batch in train_loader:
             x = batch["array"]
-            x = torch.stack(x).to(device)
-            x = x.unsqueeze(1)
-            x = x.permute(2, 1, 0)
+            x = (
+                torch.Tensor(compute_mfcc(np.array(x), 16_000))
+                .permute(0, 2, 1)
+                .to(device)
+            )
             y = batch["label"].to(device)
             y_pred = model(x.float())
             loss = criterion(y_pred, y)
@@ -122,28 +156,27 @@ def main(config):
             train_accuracy += calculate_accuracy(y_pred, y)
         train_loss /= len(train_loader)
         train_accuracy = train_accuracy / len(train_loader)
-        print(
-            f"Epoch {epoch} train loss: {train_loss}, train accuracy: {train_accuracy}"
-        )
+
         model.eval()
         with torch.no_grad():
             val_loss = 0
             val_accuracy = 0
             for batch in valid_loader:
                 x = batch["array"]
-                x = torch.stack(x).to(device)
-                x = x.unsqueeze(1)
-                x = x.permute(2, 1, 0)
+                x = (
+                    torch.Tensor(compute_mfcc(np.array(x), 16_000))
+                    .permute(0, 2, 1)
+                    .to(device)
+                )
                 y = batch["label"].to(device)
                 y_pred = model(x.float())
                 val_loss += criterion(y_pred, y).item()
                 val_accuracy += calculate_accuracy(y_pred, y)
             val_loss /= len(valid_loader)
             val_accuracy = val_accuracy / len(valid_loader)
-            print(f"Epoch {epoch} val loss: {val_loss}, val accuracy: {val_accuracy}")
             if val_loss < best_val_loss:
                 best_val_loss = val_loss
-                torch.save(model.state_dict(), "best_model.pth")
+                torch.save(model.state_dict(), f"{model_path}/best_model.pth")
             row = pd.DataFrame(
                 {
                     "epoch": [epoch],
@@ -154,7 +187,34 @@ def main(config):
                 }
             )
             results = pd.merge(results, row, how="outer")
-    results.to_csv(f"results_{model._get_name()}_{NUM_LAYERS}_{SEED}.csv", index=False)
+        print(
+            f"Epoch {epoch} train loss: {train_loss}, train accuracy: {train_accuracy} val loss: {val_loss}, val accuracy: {val_accuracy}"
+        )
+    results.to_csv(f"{model_path}/results.csv", index=False)
+
+    model.eval()
+    test_accuracy = 0.0
+    predictions = []
+    true_labels = []
+    with torch.no_grad():
+        for batch in test_loader:
+            x = batch["array"]
+            x = (
+                torch.Tensor(compute_mfcc(np.array(x), 16_000))
+                .permute(0, 2, 1)
+                .to(device)
+            )
+            y = batch["label"].to(device)
+            y_pred = model(x.float())
+            test_accuracy += calculate_accuracy(y_pred, y)
+            predictions.append(y_pred)
+            true_labels.append(y)
+    test_accuracy = test_accuracy / len(test_loader)
+
+    print(f"Test accuracy: {test_accuracy}")
+
+    with open(f"{model_path}/test_results.json", "w") as f:
+        json.dump({"test_accuracy": test_accuracy}, f, indent=4)
 
 
 if __name__ == "__main__":
