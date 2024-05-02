c6c7b80893676007ee293ec7057b75a081b65b4a
galkowskim <mikolaj.galkowski7@gmail.com>
2024-05-02 18:59:43 +0200

    add whisper files

--- experiments/whisper/main_trainer.py
+++ experiments/whisper/main_trainer.py
@@ -1,22 +1,113 @@
 import json
-import pandas as pd
-from datetime import datetime
+import random
+import warnings
+from math import ceil
 
-import evaluate
 import numpy as np
+import pandas as pd
 import torch
-from datasets import load_dataset
-from transformers import (AutoFeatureExtractor,
-                          AutoModelForAudioClassification, Trainer,
-                          TrainingArguments, WhisperConfig)
+from datasets import load_dataset, concatenate_datasets, DatasetDict
+from transformers import (
+    AutoConfig,
+    AutoFeatureExtractor,
+    AutoModelForAudioClassification,
+    Trainer,
+    TrainingArguments,
+    WhisperConfig,
+)
 from utils import CustomCallback, compute_metrics
 
+warnings.filterwarnings("ignore")
+
+normal_classes = {"yes", "no", "up", "down", "left", "right", "on", "off", "stop", "go"}
+to_be_subsampled = {
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "bed",
+    "bird",
+    "cat",
+    "dog",
+    "happy",
+    "house",
+    "marvin",
+    "sheila",
+    "tree",
+    "wow",
+    "backward",
+    "forward",
+    "follow",
+    "learn",
+    "visual",
+}
+
+def swap_to_unknown(examples):
+    examples["label"] = [10] * len(examples["label"])
+    return examples
+
+
+def swap_to_silence(examples):
+    examples["label"] = [11] * len(examples["label"])
+    return examples
+
+
+def cut_audio(example):
+    if len(example["audio"]["array"]) > 16000:
+        example["audio"]["array"] = example["audio"]["array"][:16000]
+    return example
+
+
+def swap_labels_for_data_split(data):
+    id2label_org = data.features["label"].int2str
+    normal = data.filter(lambda x: id2label_org(x["label"]) in normal_classes)
+
+    class_size = ceil(len(normal) / len(normal_classes))
+    to_subsample = data.filter(lambda x: id2label_org(x["label"]) in to_be_subsampled)
+    silence = data.filter(lambda x: id2label_org(x["label"]) == "_silence_")
+
+    silence = silence.map(cut_audio)
+    to_subsample = to_subsample.map(swap_to_unknown, batched=True)
+
+    if len(to_subsample) > class_size:
+        to_subsample = to_subsample.shuffle(seed=42).select(range(class_size))
+
+    to_subsample = to_subsample.map(swap_to_unknown, batched=True)
+    silence = silence.map(swap_to_silence, batched=True)
+    results = concatenate_datasets([normal, to_subsample, silence])
+    return results
+
+def preprocess(speech_data):
+    train = speech_data["train"]
+    validation = speech_data["validation"]
+    test = speech_data["test"]
+
+    train = swap_labels_for_data_split(train)
+    validation = swap_labels_for_data_split(validation)
+    test = swap_labels_for_data_split(test)
+
+    return DatasetDict({
+        "train": train,
+        "validation": validation,
+        "test": test
+    })
 
 def main(config):
     HG_MODEL_PATH = config["hg_model_path"]
     BATCH_SIZE = config["batch_size"]
     NUM_EPOCHS = config["num_epochs"]
     SEED = config["seed"]
+
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     feature_extractor = AutoFeatureExtractor.from_pretrained(HG_MODEL_PATH)
@@ -32,7 +123,7 @@ def main(config):
         "speech_commands", "v0.02", cache_dir="../../cache_data_whole"
     )
 
-    speech_data = speech_data.filter(lambda x: len(x["audio"]["array"]) / 16_000 < 10)
+    speech_data = preprocess(speech_data)
 
     speech_data_processed = speech_data.map(
         preprocess_function,
@@ -40,32 +131,52 @@ def main(config):
         remove_columns=["file", "audio", "speaker_id", "utterance_id", "is_unknown"],
     ).with_format("torch")
 
-    labels = speech_data["train"].features["label"].names
-    label2id, id2label = dict(), dict()
-    for i, label in enumerate(labels):
-        label2id[label] = str(i)
-        id2label[str(i)] = label
 
-    num_labels = len(id2label)
-    model = AutoModelForAudioClassification.from_pretrained(
-        HG_MODEL_PATH,
-        num_labels=num_labels,
-        label2id=label2id,
-        id2label=id2label,
-        ignore_mismatched_sizes=True,
+    id2label = {
+        "0": "yes",
+        "1": "no",
+        "2": "up",
+        "3": "down",
+        "4": "left",
+        "5": "right",
+        "6": "on",
+        "7": "off",
+        "8": "stop",
+        "9": "go",
+        "10": "_unknown_",
+        "11": "_silence_",
+    }
+    label2id = {v: k for k, v in id2label.items()}
+
+    if config["pretrained"]:
+        num_labels = len(id2label)
+        model = AutoModelForAudioClassification.from_pretrained(
+            HG_MODEL_PATH,
+            num_labels=num_labels,
+            label2id=label2id,
+            id2label=id2label,
+            ignore_mismatched_sizes=True,
+        )
+
+        # shorten the model embeddings to make it accept the input size (80, 100)
+        # https://github.com/huggingface/transformers/issues/25744
+        state_dict = model.state_dict()
+        state_dict["encoder.embed_positions.weight"] = state_dict[
+            "encoder.embed_positions.weight"
+        ][:50, :]
+        config = WhisperConfig.from_pretrained(HG_MODEL_PATH, max_source_positions=50)
+        model = AutoModelForAudioClassification.from_config(config)
+        model.load_state_dict(state_dict)
+    else:
+        config = AutoConfig.from_pretrained(HG_MODEL_PATH, max_source_positions=50)
+        config.id2label = id2label
+        config.label2id = label2id
+        model = AutoModelForAudioClassification.from_config(config)
+
+    pretrained = "pretrained" if config["pretrained"] else "not_pretrained"
+    model_path = (
+        f"{pretrained}/{HG_MODEL_PATH.replace('/', '_').replace('-', '_')}_seed_{SEED}"
     )
-
-    # shorten the model embeddings to make it accept the input size (80, 100)
-    # https://github.com/huggingface/transformers/issues/25744
-    state_dict = model.state_dict()
-    state_dict["encoder.embed_positions.weight"] = state_dict[
-        "encoder.embed_positions.weight"
-    ][:50, :]
-    config = WhisperConfig.from_pretrained(HG_MODEL_PATH, max_source_positions=50)
-    model = AutoModelForAudioClassification.from_config(config)
-    model.load_state_dict(state_dict)
-
-    model_path = f"{HG_MODEL_PATH.replace('/', '_').replace('-', '_')}_seed_{SEED}"
     training_args = TrainingArguments(
         output_dir=f"checkpoints/{model_path}",
         evaluation_strategy="epoch",
@@ -104,7 +215,6 @@ def main(config):
         if "train_accuracy" in dictionary
     ]
     train_losses = pd.DataFrame(train_losses)
-    print(train_losses)
     train_losses.epoch = train_losses.epoch.astype(int)
 
     val_losses = [
@@ -127,6 +237,8 @@ def main(config):
     with open(f"checkpoints/{model_path}/test_metrics.json", "w") as f:
         json.dump(output.metrics, f, indent=4)
 
+    with open(f"checkpoints/{model_path}/config.json", "w") as f:
+        json.dump(config, f, indent=4)
 
 if __name__ == "__main__":
     with open("config.json") as f:
